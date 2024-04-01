9f8760532d636788e7baaf767d57f70048e7f46d
Loice-Staice <staiceloicea@gmail.com>
2024-04-01 22:21:54 +0300

    first commit

---
+++ function.js
@@ -0,0 +1,5 @@
+var scrolls= document.getElementById("scrolls")
+var scrollleft= document.getElementById("scroll-left")
+var scrollright= document.getElementById("scroll-right")
+scrollleft.addEventListener("click",(Event)=>{scrolls.scrollBy(800,0);});
+scrollright.addEventListener("click",(Event)=>{scrolls.scrollBy(800,0);});
